5936ed77291d4e21bfb936181e5c3fd27b355cea
choi <chlguddn7@gmail.com>
2022-07-11 17:33:20 +0900

    7. 11.(월) 5단원 연습문제 풀이

--- src/Main.java
+++ src/Main.java
@@ -5,7 +5,7 @@ public class Main {
 
     public static void main(String[] args) throws Exception{
         subject5 sub = new subject5();
-        sub.Exercise5_12();
+        sub.Exercise5_13();
     }
 
 }
--- src/Subject/subject5.java
+++ src/Subject/subject5.java
@@ -222,6 +222,25 @@ public class subject5 {
         String[] words = { "television", "computer", "mouse", "phone" };
 
         Scanner scanner = new Scanner(System.in);
+
+        for(int i=0;i<words.length;i++) {
+            char[] question = words[i].toCharArray();
+
+            for(int j=0; j< question.length; j++) {
+                int index = (int) (Math.random() * (question.length));
+
+                char temp = question[index];
+                question[index] = question[j];
+                question[j] = temp;
+            }
+
+            System.out.printf("Q%d. %s의 정답을 입력하세요.>", i+1, new String(question));
+            String answer = scanner.nextLine();
+            if(words[i].equals(answer.trim()))
+                System.out.printf("맞았습니다.%n%n");
+            else
+                System.out.printf("틀렸습니다.%n%n");
+        }
     }
 
 }
