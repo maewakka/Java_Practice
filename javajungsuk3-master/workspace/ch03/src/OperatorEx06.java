ea555e56af2e5373715eb6b22213d16fed6a1953
choi <chlguddn7@gmail.com>
2022-07-11 17:22:32 +0900

    gitignore 다시 적용

--- javajungsuk3-master/workspace/ch03/src/OperatorEx06.java
+++
@@ -1,9 +0,0 @@
-class OperatorEx06 { 
-      public static void main(String[] args) { 
-            byte a = 10; 
-            byte b = 20; 
-            byte c = a + b; 
-
-            System.out.println(c); 
-      } 
-} 
